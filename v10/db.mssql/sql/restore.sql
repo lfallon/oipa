c50fe417bf59fe2590746e958418d6d13f8ecdca
Jerome Bridge <jeromebridge@gmail.com>
2019-01-28 19:26:01 -0500

    Working on MSSQL restore script

--- v10/db.mssql/sql/restore.sql
+++ v10/db.mssql/sql/restore.sql
@@ -4,13 +4,28 @@ SET NOCOUNT ON;
 -- :setvar ACTION 'execute'
 DECLARE @Action VARCHAR(1000) = N'$(ACTION)'
 -- DECLARE @Action VARCHAR(1000) = 'names'
-DECLARE @BackupPath VARCHAR(1000) = '/restore'
+DECLARE @BackupPath VARCHAR(1000) = N'$(BACKUP_PATH)'
+-- DECLARE @BackupPath VARCHAR(1000) = '/scratch'
 DECLARE @DataPath VARCHAR(1000) = '/var/opt/mssql/data'
+-- File Or Directory
+DECLARE @FileExists TABLE( FileExists INT, IsDirectory INT, ParentDirExists INT )
+INSERT INTO @FileExists
+  EXEC master..xp_fileexist @BackupPath
+DECLARE @IsFile INT
+SELECT @IsFile = FileExists FROM @FileExists
+
+
 -- List Of Files
 DECLARE @FileList TABLE( BackupFile VARCHAR(1000), DEPTH INT, FILEFLAG bit )
 DECLARE @Cmd VARCHAR(1000) = 'DIR /b ' + @BackupPath
 INSERT INTO @FileList( BackupFile, Depth, FileFlag )
- EXEC master..xp_dirtree @BackupPath, 10, 1
+  EXEC master..xp_dirtree @BackupPath, 10, 1
+UPDATE @FileList SET BackupFile = @BackupPath + '/' + BackupFile
+IF @IsFile = 1
+BEGIN
+  INSERT INTO @FileList VALUES( @BackupPath, 1, 1 )
+END
+
 -- Loop Through Backup Files
 DECLARE @BackupFileList TABLE( LogicalName VARCHAR(64), PhysicalName VARCHAR(130), [Type] VARCHAR(1),
  FileGroupName VARCHAR(64), Size DECIMAL(20, 0), MaxSize DECIMAL(25,0), FileID bigint, CreateLSN DECIMAL(25,0),
@@ -40,13 +55,13 @@ DECLARE @DataFile VARCHAR(1000)
 DECLARE @LogFile VARCHAR(1000)
 DECLARE @DatabaseName VARCHAR(1000)
 DECLARE @RestoreSql NVARCHAR(4000)
-DECLARE @RestoreTable TABLE( DatabaseName VARCHAR(1000), RestoreSql NVARCHAR(4000) )
+DECLARE @RestoreTable TABLE( FilePath VARCHAR(1000), DatabaseName VARCHAR(1000), RestoreSql NVARCHAR(4000) )
 DECLARE db_cursor CURSOR FOR SELECT BackupFile FROM @FileList WHERE BackupFile LIKE '%.BAK'
 OPEN db_cursor
 FETCH NEXT FROM db_cursor INTO @BackupFileName
 WHILE @@FETCH_STATUS = 0
 BEGIN
-  SET @BackupFilePath = @BackupPath + '/' + @BackupFileName
+  SET @BackupFilePath = @BackupFileName
   DELETE FROM @BackupFileList
   DELETE FROM @BackupHeader
   INSERT INTO @BackupFileList EXEC('RESTORE FILELISTONLY FROM DISK = ''' + @BackupFilePath + '''' )
@@ -56,7 +71,7 @@ BEGIN
   SELECT @RestoreSql = COALESCE( @RestoreSql + ', ', '') + 'MOVE ''' + LogicalName + ''' TO ''' + @DataPath + '/' + @DatabaseName + '.' + LogicalName + '.mdf''' FROM @BackupFileList WHERE Type='D'
   SELECT @RestoreSql = COALESCE( @RestoreSql + ', ', '') + 'MOVE ''' + LogicalName + ''' TO ''' + @DataPath + '/' + @DatabaseName + '.' + LogicalName + '.ldf''' FROM @BackupFileList WHERE Type='L'
   SET @RestoreSql = 'RESTORE DATABASE ' + @DatabaseName + ' FROM DISK = ''' + @BackupFilePath + ''' WITH REPLACE, ' + @RestoreSql
-  INSERT INTO @RestoreTable VALUES( @DatabaseName, @RestoreSql )
+  INSERT INTO @RestoreTable VALUES( @BackupFilePath, @DatabaseName, @RestoreSql )
 
   FETCH NEXT FROM db_cursor INTO @BackupFileName
 END
